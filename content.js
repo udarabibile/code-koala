d95150041c01b6b7825818bb3e6453d661e50a8e
Udara Bibile <bibile.13@cse.mrt.ac.lk>
2019-09-22 08:31:49 +0530

    Tags page

---
+++ content.js
@@ -0,0 +1,65 @@
+var udaraBibile = {
+    name: "Udara Bibile",
+    role: "Full Stack Developer",
+    pic: "images/pic01.jpg"
+}
+
+var tags = [
+    { id: "nodejs", icon: "fa-download"},
+    { id: "reactjs", icon: "fa-download"},
+    { id: "sql", icon: "fa-download"},
+]
+
+var post_sql_foreign_keys = {
+    id: "sql-foreign-keys/index.html",
+    title: "SQL Snippet: Foreign Keys",
+    subTitle: "Making relationship between database tables",
+    date: "September 21, 2019",
+    author: udaraBibile.name,
+    featureImage: "images/pic01.jpg",
+    summary: "xxxxxxxxssssxxxxxxxx xxxxxxxxssssxxxxxxxx xxxxxxxxssssxxxxxxxx xxxxxxxxssssxxxxxxxx.",
+    tags: [ tags[0].id, tags[1].id],
+}
+
+var post_sql_transaction = {
+    id: "sql-foreign-keys/index.html",
+    title: "SQL Snippet: Transaction",
+    subTitle: "Making relationship between database tables",
+    date: "September 21, 2019",
+    author: udaraBibile.name,
+    featureImage: "images/pic01.jpg",
+    summary: "xxxxxxxxssssxxxxxxxx xxxxxxxxssssxxxxxxxx xxxxxxxxssssxxxxxxxx xxxxxxxxssssxxxxxxxx.",
+    tags: [ tags[0].id, tags[2].id],
+}
+
+var content = {
+    posts: [
+        post_sql_foreign_keys,
+        post_sql_transaction
+    ],
+
+    authors: [
+        udaraBibile
+    ],
+
+    tags: tags
+}
+
+
+/*
+    http://jsfiddle.net/aybalasubramanian/N2b5M/1/
+
+    person: { firstName: "Garry", lastName: "Finch"},
+
+    Handlebars.registerHelper('createTags', (tags) => {
+        return person.firstName + " " + person.lastName;
+    });
+
+    {{#users}} 
+    <tr> 
+        <td>{{fullName person}}</td> 
+        <td>{{jobTitle}}</td> 
+        <td><a href="https://twitter.com/{{twitter}}">@{{twitter}}</a></td> 
+    </tr> 
+    {{/users}} 
+*/
